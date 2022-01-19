e4b888e42a2bc98050450ee0950831ebffe11eae
gianni <gianni@rebrandly.com>
2022-01-19 14:28:51 +0100

    move alias inputbox to partials

---
+++ admin/partials/rebrandly-domain-redirect-alias-field.php
@@ -0,0 +1,12 @@
+<?php
+    $options = get_option( 'rebrandly_aliasing_options' );
+    echo "
+        <input 
+            id='rebrandly_aliasing_plugin_setting_alias'
+            name='rebrandly_aliasing_options[alias]'
+            class='rb-aliasing-alias'
+            placeholder='e.g. 46e8ac9583d7e0c513485aef06045733.rebrandly.cc'
+            type='text'
+            value='" . esc_attr( $options['alias'] ) . "' 
+        />";
+?>
